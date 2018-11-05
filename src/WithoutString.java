86c7ddaf5ed7caab52365bc9fbf1d16401355b4f
vikram-tandon-4 <vikramtandon4192@gmail.com>
2018-11-05 01:51:25 -0600

    Without String

---
+++ src/WithoutString.java
@@ -0,0 +1,19 @@
+
+public class WithoutString {
+	/*
+	 * Given two strings, base and remove, return a version of the base string where
+	 * all instances of the remove string have been removed (not case sensitive).
+	 * You may assume that the remove string is length 1 or more. Remove only
+	 * non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+	 * withoutString("Hello there", "llo") => "He there"
+	 * withoutString("Hello there", "e") => "Hllo thr" withoutString("Hello there",
+	 * "x") => "Hello there"
+	 */
+	public static void main(String[] args) {
+		System.out.println(withoutString("This is a FISH", "IS"));
+	}
+
+	public static String withoutString(String base, String remove) {
+		return base.replaceAll("(?i)" + remove, "");
+	}
+}
