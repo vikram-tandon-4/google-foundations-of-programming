dc0ed594c9f8613922c93bbc7a396325f7d37069
vikram-tandon-4 <vikramtandon4192@gmail.com>
2018-11-28 02:33:50 -0600

    Fix 34

---
+++ src/Fix34.java
@@ -0,0 +1,49 @@
+
+/*
+ Return an array that contains exactly the same numbers as the given array,
+  but rearranged so that every 3 is immediately followed by a 4. Do not move the 3's,
+   but every other number may move. The array contains the same number of 3's and 4's,
+    every 3 has a number after it that is not a 3, and a 3 appears in the array before any 4.
+
+
+fix34([1, 3, 1, 4]) => [1, 3, 4, 1]
+fix34([1, 3, 1, 4, 4, 3, 1]) => [1, 3, 4, 1, 1, 3, 4]
+fix34([3, 2, 2, 4]) => [3, 4, 2, 2]
+ */
+
+import java.util.ArrayList;
+
+public class Fix34 {
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 3, 1, 4, 4, 3, 1 };
+		int[] nums34 = fix34(nums);
+
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums34[i] + " ");
+		}
+	}
+
+	public static int[] fix34(int[] nums) {
+
+		ArrayList<Integer> foursPosition = new ArrayList<Integer>();
+		int currentFour = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 4) {
+				foursPosition.add(i);
+			}
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 3) {
+				int temp = nums[i + 1];
+				nums[i + 1] = nums[foursPosition.get(currentFour)];
+				nums[foursPosition.get(currentFour)] = temp;
+				currentFour++;
+			}
+		}
+		
+		return nums;
+	}
+
+}
