be751f25ae80bee625918e92e11597f12518aabd
vikram-tandon-4 <vikramtandon4192@gmail.com>
2018-12-10 23:51:30 -0600

    Collapse Duplicates

---
+++ src/CollapseDuplicates.java
@@ -0,0 +1,33 @@
+/*
+ 
+Fix this duplicate collapsing code: public String collapseDuplicates(String a) 
+{ int i = 0; String result = ""; while (i < a.length())
+ { char ch = a.charAt(i); result += ch; while (a.charAt(i+1) == ch) { i++; } i++; } return result; }
+
+
+collapseDuplicates("a") => "a"
+collapseDuplicates("aa") => "a"
+collapseDuplicates("abc") => "abc"
+ */
+public class CollapseDuplicates {
+
+public static void main(String arg[]) {
+		
+		System.out.print(collapseDuplicates("abbbcaaa"));
+	}
+	
+	public static String collapseDuplicates(String a) {
+		   int i = 0;
+		   String result = "";
+		   while (i < a.length()) {
+		     char ch = a.charAt(i);
+		     result += ch;
+		     while (i+1< a.length() && a.charAt(i+1) == ch) {
+		       i++; 
+		       
+		     } 
+		     i++;
+		     } 
+		   return result;
+		}
+}
